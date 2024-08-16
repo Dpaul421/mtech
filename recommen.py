f8a7413d120d8021e97d0834cb7c94fdfa5f4fa9
Dpaul421 <debanjan.admsm@gmail.com>
2024-08-16 22:14:42 +0530

    Arithmetic operator update recommen.py
    The previous version recommended 1-29 movies, we increased it by one and made it 1-30

--- recommen.py
+++ recommen.py
@@ -66,7 +66,7 @@ i = 1
 for movie in sorted_similar_movies:
   index = movie[0]
   title_from_index = movies_data[movies_data.index==index]['title'].values[0]
-  if (i<30):
+  if (i<=30):
     print(i, '.',title_from_index)
     i+=1
     
